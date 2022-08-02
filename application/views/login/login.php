55d1976f4a2f4deff6f8e3461aae8a50e68d30fd
Davies John Boco <57469769+djTenTen@users.noreply.github.com>
2022-08-02 16:57:26 +0800

    login page new design

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -36,35 +36,35 @@
 
 
 </head>
-<body class="bg-gradient-primary">
-
-	<div class="container">
-		<!-- Outer Row -->
-		<div class="row justify-content-center">
-			<div class="col-xl-10 col-lg-12 col-md-9" >
-				<div class="card o-hidden border-0 shadow-lg my-5" style="background-image: url(<?= base_url();?>img/hccnewlypainted2.jpg); height: auto;
+<body style="background-image: url(<?= base_url();?>img/hccnewlypainted2.jpg); height: auto;
                 width: auto;
                 background-attachment: fixed;
                 background-position: center;
                 background-size: cover;
                 overflow-x: hidden; ">
+
+	<div class="container">
+		<!-- Outer Row -->
+		<div class="row justify-content-center">
+			<div class="col-xl-10 col-lg-12 col-md-9" >
+				<div class="card o-hidden border-0 shadow-lg my-5 bg-gradient-primary opacity-50">
 					<div class="card-body p-0">
 						<!-- Nested Row within Card Body -->
 						<div class="row">
 							<div class="col-lg-6 d-none d-lg-block">
-                                <img src="<?= base_url();?>img/logo4.png" alt="Logo" class="" style="height:500px;">
+                                <img src="<?= base_url();?>img/logo4.png" alt="Logo" class="" style="height:450px;">
 							</div>
 							
 							<div class="col-lg-6">
 								<div class="p-5">
 									<div class="text-center">
-										<h3 class="text-white mb-4">Welcome to School Management System!</h3>
+										<h3 class="text-white mb-4">Holy Cross College School Management System!</h3>
 									</div>
 									<form class="user" action="authenticate" method="post">
 										<div class="form-group">
 											<input type="email" class="form-control form-control-user"
 												id="exampleInputEmail" aria-describedby="emailHelp"
-												placeholder="Enter Email Address..."  name="un" required>
+												placeholder="Username"  name="un" required>
 										</div>
 										<div class="form-group">
 											<input type="password" class="form-control form-control-user"
