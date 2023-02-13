4706bed05411405cdb435e0ad5736e7958e28247
Davies John Boco <57469769+djTenTen@users.noreply.github.com>
2023-02-13 11:37:19 +0800

    remove create account and forgot pass

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -82,12 +82,12 @@
 										</a> -->
 									</form>
 									<hr>
-									<div class="text-center">
+									<!-- <div class="text-center">
 										<a class="small" href="forgot-password.html">Forgot Password?</a>
 									</div>
 									<div class="text-center">
 										<a class="small" href="selectdpt">Create an Account!</a>
-									</div>
+									</div> -->
 								</div>
 							</div>
 						</div>
